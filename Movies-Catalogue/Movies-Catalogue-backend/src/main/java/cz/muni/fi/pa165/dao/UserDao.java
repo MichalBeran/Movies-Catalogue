de33ec5c57d16f862dd9eead20f37ff9295ba201
MichalBeran <beran.mi.g@gmail.com>
2017-10-26 23:35:27 +0200

    User dao (#25)
    * add Dao for User entity

---
+++ Movies-Catalogue/Movies-Catalogue-backend/src/main/java/cz/muni/fi/pa165/dao/UserDao.java
@@ -0,0 +1,48 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entities.User;
+
+import java.util.List;
+
+/**
+ * @author Marek
+ *
+ */
+public interface UserDao {
+
+    /**
+     * Persits a newly created entity
+     *
+     * @param entity entity to create
+     */
+    void create(User entity);
+
+    /**
+     * Persits a newly created entity
+     *
+     * @param entity entity to create
+     */
+    void update(User entity);
+
+    /**
+     * Gets all entities from the repository
+     *
+     * @return List<User> a list of entities
+     */
+    List<User> findAll();
+
+    /**
+     * Finds entity with corresponding id
+     *
+     * @param id unique identifier of entity
+     * @return User the found entity
+     */
+    User findById(long id);
+
+    /**
+     * Removes specified entity from the repository
+     *
+     * @param id id of entity
+     */
+    void delete(long id);
+}

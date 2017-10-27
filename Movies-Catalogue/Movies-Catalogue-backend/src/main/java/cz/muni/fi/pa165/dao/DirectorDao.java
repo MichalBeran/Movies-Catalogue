ae22aea3c3153b3afe5737e91a2ccbe4a9db818f
MichalBeran <beran.mi.g@gmail.com>
2017-10-27 21:06:54 +0200

    Created Actor Dao and Director Dao [#8] (#32)
    * added Actor Dao and Director Dao
    
    * removed unused import

---
+++ Movies-Catalogue/Movies-Catalogue-backend/src/main/java/cz/muni/fi/pa165/dao/DirectorDao.java
@@ -0,0 +1,46 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entities.Director;
+
+import java.util.List;
+
+/**
+ * Created by Michal on 27.10.2017.
+ */
+public interface DirectorDao {
+    /**
+     * Persists a newly created entity
+     *
+     * @param entity entity to create
+     */
+    void create(Director entity);
+
+    /**
+     * Update saved entity
+     *
+     * @param entity entity to create
+     */
+    void update(Director entity);
+
+    /**
+     * Gets all entities from the repository
+     *
+     * @return List<Director> a list of entities
+     */
+    List<Director> findAll();
+
+    /**
+     * Finds entity with corresponding id
+     *
+     * @param id unique identifier of entity
+     * @return Director the found entity
+     */
+    Director findById(long id);
+
+    /**
+     * Removes specified entity from the repository
+     *
+     * @param id id of entity
+     */
+    void delete(long id);
+}

547f31bc82d4e4cb0df82d5e4b00569a9fc9eae1
Maroš Grman <maros.gr@centrum.sk>
2017-10-28 13:55:55 +0200

    Clean unused imports

--- Movies-Catalogue/Movies-Catalogue-backend/src/main/java/cz/muni/fi/pa165/entities/Rating.java
+++ Movies-Catalogue/Movies-Catalogue-backend/src/main/java/cz/muni/fi/pa165/entities/Rating.java
@@ -5,14 +5,12 @@
  */
 package cz.muni.fi.pa165.entities;
 
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
+
 /**
  *
  * @author Maros

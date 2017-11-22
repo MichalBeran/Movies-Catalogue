b4dfd0a5924478863fbcf2ecbb760383b3ac3ce4
MichalBeran <beran.mi.g@gmail.com>
2017-11-22 14:37:38 +0100

    Added empty user facade and service (#57)

---
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/main/java/cz/muni/fi/pa165/service/UserService.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.service;
+
+/**
+ * @author Michal
+ */
+public interface UserService {
+}
---
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.service;
+
+/**
+ * @author Michal
+ */
+public class UserServiceImpl implements UserService {
+}

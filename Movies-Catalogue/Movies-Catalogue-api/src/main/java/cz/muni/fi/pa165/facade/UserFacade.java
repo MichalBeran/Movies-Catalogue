536e89fd9f1f614b007347fdd27a35720bc91abd
MichalBeran <beran.michal@mail.muni.cz>
2017-11-22 14:35:45 +0100

    Added empty user facade and service

---
+++ Movies-Catalogue/Movies-Catalogue-api/src/main/java/cz/muni/fi/pa165/facade/UserFacade.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.facade;
+
+/**
+ * @author Michal
+ */
+public interface UserFacade {
+}
---
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/main/java/cz/muni/fi/pa165/facade/UserFacadeImpl.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.facade;
+
+/**
+ * @author Michal
+ */
+public class UserFacadeImpl implements UserFacade{
+    
+}

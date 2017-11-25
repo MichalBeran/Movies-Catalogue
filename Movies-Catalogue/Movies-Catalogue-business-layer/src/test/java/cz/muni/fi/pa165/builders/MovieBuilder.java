1cb068369e1428722c3c86b7c245b4068bcb8f18
Dominik Mlynka <dominik.mlynka@gmail.com>
2017-11-25 02:07:44 +0100

    MovieBuilder added(for testing)

---
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/test/java/cz/muni/fi/pa165/builders/MovieBuilder.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.builders;
+
+import cz.muni.fi.pa165.entities.Actor;
+import cz.muni.fi.pa165.entities.Director;
+import cz.muni.fi.pa165.entities.Genre;
+import cz.muni.fi.pa165.entities.Movie;
+import cz.muni.fi.pa165.entities.Rating;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Dominik
+ */
+public class MovieBuilder {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String image;
+
+    private LocalDate dateOfRelease;
+
+    private Set<Genre> genres;
+
+    private Set<Actor> actors;
+    
+    private Rating rating;
+
+    private Director director;
+    
+    
+    public MovieBuilder() {
+        genres = new HashSet();
+        actors = new HashSet();
+    }
+    
+    public MovieBuilder id(Long id){
+        this.id = id;
+        return this;
+    }
+    
+    public MovieBuilder title(String title){
+        this.title = title;
+        return this;
+    }
+    
+    public MovieBuilder description(String description){
+        this.description = description;
+        return this;
+    }
+    
+    public MovieBuilder image(String image){
+        this.image = image;
+        return this;
+    }
+    
+    public MovieBuilder dateOfRelase(LocalDate date){
+        this.dateOfRelease = date;
+        return this;
+    }
+    
+    public MovieBuilder genres(Genre genre){
+        this.genres.add(genre);
+        return this;
+    }
+    
+    public MovieBuilder actor(Actor actors){
+        this.actors.add(actors);
+        return this;
+    }
+    
+    public MovieBuilder rating(Rating rating){
+        this.rating = rating;
+        return this;
+    }
+    
+    public MovieBuilder director(Director director){
+        this.director = director;
+        return this;
+    }
+    
+    public Movie build(){
+        Movie entity = new Movie();
+        entity.setId(id);
+        entity.setTitle(title);
+        entity.setDescription(description);
+        entity.setImage(image);
+        entity.setDateOfRelease(dateOfRelease);
+        if(!genres.isEmpty())
+        entity.setGenres(genres);
+        if(!actors.isEmpty())
+        entity.setActors(actors);
+        entity.setRating(rating);
+        entity.setDirector(director);
+        return null;
+    }
+}

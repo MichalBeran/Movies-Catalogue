4f80a27f5f3e55f638d57a4b8d3164cc032e8f9b
MichalBeran <beran.michal@mail.muni.cz>
2017-11-25 21:29:04 +0100

    Added facade implementation.

--- Movies-Catalogue/Movies-Catalogue-business-layer/src/main/java/cz/muni/fi/pa165/facade/UserFacadeImpl.java
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/main/java/cz/muni/fi/pa165/facade/UserFacadeImpl.java
@@ -1,8 +1,67 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dao.UserDao;
+import cz.muni.fi.pa165.dto.UserDto;
+import cz.muni.fi.pa165.entities.User;
+import cz.muni.fi.pa165.mapping.BeanMappingService;
+import cz.muni.fi.pa165.service.UserService;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * @author Michal
  */
 public class UserFacadeImpl implements UserFacade{
-    
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BeanMappingService mapper;
+
+    @Override
+    public void registerUser(UserDto u, String password) {
+        userService.registerUser(mapper.mapTo(u, User.class), password);
+    }
+
+    @Override
+    public boolean signIn(UserDto u, String password) {
+        return userService.signIn(mapper.mapTo(u, User.class), password);
+    }
+
+    @Override
+    public boolean isAdmin(UserDto u) {
+        return userService.isAdmin(mapper.mapTo(u, User.class));
+    }
+
+    @Override
+    public List<UserDto> findAllUsers() {
+        return mapper.mapTo(userService.findAllUsers(), UserDto.class);
+    }
+
+    @Override
+    public UserDto findUserById(Long id) {
+        return mapper.mapTo(userService.findUserById(id), UserDto.class);
+    }
+
+    @Override
+    public UserDto findUserByMail(String mail) {
+        return mapper.mapTo(userService.findUserByMail(mail), UserDto.class);
+    }
+
+    @Override
+    public UserDto findUserByNick(String nick) {
+        return mapper.mapTo(userService.findUserByNick(nick), UserDto.class);
+    }
+
+    @Override
+    public UserDto update(UserDto u, String password) {
+        return mapper.mapTo(userService.update(mapper.mapTo(u, User.class), password), UserDto.class);
+    }
+
+    @Override
+    public void delete(UserDto u) {
+        userService.delete(mapper.mapTo(u, User.class));
+    }
 }

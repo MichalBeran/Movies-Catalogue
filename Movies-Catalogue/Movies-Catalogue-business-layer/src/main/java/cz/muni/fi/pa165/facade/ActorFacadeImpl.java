d9e3a38f753554eb1bab087129ae07b88a0a1d9b
Maroš Grman <maros.gr@centrum.sk>
2017-11-26 19:01:27 +0100

    test commit

---
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/main/java/cz/muni/fi/pa165/facade/ActorFacadeImpl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.facade;
+
+import java.util.Date;
+import java.util.List;
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ *
+ * @author Maros
+ */
+public class ActorFacadeImpl {
+    
+}

76ba7a615f68f1cbf6e9d9c7c28a9b883c0aab64
Maroš Grman <maros.gr@centrum.sk>
2017-11-27 16:43:31 +0100

    Added actor and director builders

---
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/test/java/cz/muni/fi/pa165/builders/ActorBuilder.java
@@ -0,0 +1,56 @@
+package cz.muni.fi.pa165.builders;
+
+import java.time.LocalDate;
+import cz.muni.fi.pa165.entities.Movie;
+import cz.muni.fi.pa165.entities.Actor;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Maros on 11/27/2017.
+ */
+public class ActorBuilder {
+
+
+    private Actor actor;
+
+    public ActorBuilder(){
+        this.actor = new Actor();
+    }
+
+    public ActorBuilder id(Long id){
+        this.actor.setId(id);
+        return this;
+    }
+
+    public ActorBuilder firstName(String firstNameame){
+        this.actor.setFirstName(firstNameame);
+        return this;
+    }
+
+    public ActorBuilder lastName(String lastNameame){
+        this.actor.setLastName(lastNameame);
+        return this;
+    }
+
+    public ActorBuilder description(LocalDate dateOfBirth){
+        this.actor.setDateOfBirth(dateOfBirth);
+        return this;
+    }
+
+    public ActorBuilder movies(List<Movie> movies){
+        this.actor.setMovies((Set<Movie>)movies);
+        return this;
+    }
+
+    public Actor build(){
+        Actor actor = new Actor();
+        actor.setId(this.actor.getId());
+        actor.setFirstName(this.actor.getFirstName());
+        actor.setDateOfBirth(this.actor.getDateOfBirth());
+        actor.setMovies(this.actor.getMovies());
+        this.actor = new Actor();
+        return actor;
+    }
+}
---
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/test/java/cz/muni/fi/pa165/builders/DirectorBuilder.java
@@ -0,0 +1,56 @@
+package cz.muni.fi.pa165.builders;
+
+import java.time.LocalDate;
+import cz.muni.fi.pa165.entities.Movie;
+import cz.muni.fi.pa165.entities.Director;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Maros on 11/27/2017.
+ */
+public class DirectorBuilder {
+
+
+    private Director director;
+
+    public DirectorBuilder(){
+        this.director = new Director();
+    }
+
+    public DirectorBuilder id(Long id){
+        this.director.setId(id);
+        return this;
+    }
+
+    public DirectorBuilder firstName(String firstNameame){
+        this.director.setFirstName(firstNameame);
+        return this;
+    }
+
+    public DirectorBuilder lastName(String lastNameame){
+        this.director.setLastName(lastNameame);
+        return this;
+    }
+
+    public DirectorBuilder description(LocalDate dateOfBirth){
+        this.director.setDateOfBirth(dateOfBirth);
+        return this;
+    }
+
+    public DirectorBuilder movies(List<Movie> movies){
+        this.director.setMovies((Set<Movie>)movies);
+        return this;
+    }
+
+    public Director build(){
+        Director director = new Director();
+        director.setId(this.director.getId());
+        director.setFirstName(this.director.getFirstName());
+        director.setDateOfBirth(this.director.getDateOfBirth());
+        director.setMovies(this.director.getMovies());
+        this.director = new Director();
+        return director;
+    }
+}

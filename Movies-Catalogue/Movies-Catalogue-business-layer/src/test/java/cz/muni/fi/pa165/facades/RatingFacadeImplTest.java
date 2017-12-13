90bfba1d408ae3f5f13f6f04d08e29a3dfda0541
Marek Urban <marek@Mareks-MacBook-Pro.local>
2017-12-13 21:12:21 +0100

    Something has fucked up, so this fixes it

--- Movies-Catalogue/Movies-Catalogue-business-layer/src/test/java/cz/muni/fi/pa165/facades/RatingFacadeImplTest.java
+++ Movies-Catalogue/Movies-Catalogue-business-layer/src/test/java/cz/muni/fi/pa165/facades/RatingFacadeImplTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;

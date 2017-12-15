4bcb8fe6ad4bf39a0e8bfdbae8ee6cf8b19fdc26
Marek Urban <marek@Mareks-MacBook-Pro.local>
2017-12-15 17:47:56 +0100

    add ui clone to the rest module; add favicon

--- Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/RootWebContext.java
+++ Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/RootWebContext.java
@@ -8,7 +8,7 @@ import cz.muni.fi.pa165.rest.interceptors.AllowOriginInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.*;
 import cz.muni.fi.pa165.configuration.ServiceConfiguration;
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -17,9 +17,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
@@ -51,6 +48,12 @@ public class RootWebContext extends WebMvcConfigurerAdapter {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(customJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry){
+        registry.addViewController("/PA165").setViewName("forward:index.html");
+
     }
 
 }

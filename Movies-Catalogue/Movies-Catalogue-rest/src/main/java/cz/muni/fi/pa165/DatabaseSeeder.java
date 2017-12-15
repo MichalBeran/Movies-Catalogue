7b96c89524babbdd9b649066e89604274d9e5e76
arrynn <arrynn@users.noreply.github.com>
2017-12-15 21:58:24 +0100

    updated DatabaseSeeder; updated max string lengths in entities (#92)
    * add DatabaseSeeder
    
    * apply database seeder to RootWebContext
    
    * update DatabaseSeeder; update max string lengths in entities

--- Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/DatabaseSeeder.java
+++ Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/DatabaseSeeder.java
@@ -4,13 +4,9 @@ import com.google.common.io.Resources;
 import cz.muni.fi.pa165.dto.*;
 import cz.muni.fi.pa165.facade.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -57,7 +53,7 @@ public class DatabaseSeeder {
     private RatingDto adminRating;
     private RatingDto pepaRating;
 
-    public void seed(){
+    public void seed() {
         seedUsers();
         seedGenres();
         seedActors();
@@ -84,17 +80,15 @@ public class DatabaseSeeder {
         pepa.setId(userFacade.registerUser(pepa, passw));
     }
 
-    private void seedGenres(){
+        private void seedGenres() {
         adventure = new GenreDto();
         adventure.setName("Adventure");
         adventure.setDescription("Adventure films are usually exciting stories, with new experiences or exotic locales, very similar to or often paired with the action film genre.");
-        adventure.setDescription("Adventure films are usually exciting stories");
         adventure.setId(genreFacade.create(adventure));
 
         action = new GenreDto();
         action.setName("Action");
         action.setDescription("Action films usually include high energy, big-budget physical stunts and chases, possibly with rescues, battles, fights, escapes, destructive crises (floods, explosions, natural disasters, fires, etc.), non-stop motion, spectacular rhythm and pacing, and adventurous, often two-dimensional 'good-guy' heroes (or recently, heroines) battling 'bad guys' - all designed for pure audience escapism.");
-        action.setDescription("Action films usually include high energy, big-budget physical stunts and chases.");
         action.setId(genreFacade.create(action));
     }
 
@@ -102,38 +96,37 @@ public class DatabaseSeeder {
         jackie = new ActorDto();
         jackie.setFirstName("Jackie");
         jackie.setLastName("Chan");
-        jackie.setDateOfBirth(LocalDate.of(1954,4,7));
+        jackie.setDateOfBirth(LocalDate.of(1954, 4, 7));
         jackie.setId(actorFacade.create(jackie));
 
         gal = new ActorDto();
         gal.setFirstName("Gal");
         gal.setLastName("Gadot");
-        gal.setDateOfBirth(LocalDate.of(1985,4,30));
+        gal.setDateOfBirth(LocalDate.of(1985, 4, 30));
         gal.setId(actorFacade.create(gal));
     }
 
-    private void seedDirectors(){
+    private void seedDirectors() {
         martin = new DirectorDto();
         martin.setFirstName("Martin");
         martin.setLastName("Campbell");
-        martin.setDateOfBirth(LocalDate.of(1943,10,24));
+        martin.setDateOfBirth(LocalDate.of(1943, 10, 24));
+
         martin.setId(directorFacade.create(martin));
 
         patty = new DirectorDto();
         patty.setFirstName("Patty");
         patty.setLastName("Jenkins");
-        patty.setDateOfBirth(LocalDate.of(1971,6,24));
+        patty.setDateOfBirth(LocalDate.of(1971, 6, 24));
         patty.setId(directorFacade.create(patty));
     }
 
     private void seedMovies() {
-
         foreigner = new CreateMovieDto();
         foreigner.setTitle("The foreigner");
         foreigner.setDescription("A humble businessman with a buried past seeks justice when his daughter is killed in an act of terrorism. A cat-and-mouse conflict ensues with a government official, whose past may hold clues to the killers' identities.");
-        foreigner.setDescription("A humble businessman with a buried past seeks justice when his daughter is killed in an act of terrorism.");
         foreigner.setDescription("dscr");
-        foreigner.setDateOfRelease(LocalDate.of(2017,9,30));
+        foreigner.setDateOfRelease(LocalDate.of(2017, 9, 30));
         List<GenreDto> genres = new ArrayList<>();
         genres.add(action);
         foreigner.setGenres(genres);
@@ -142,15 +135,13 @@ public class DatabaseSeeder {
         foreigner.setActors(actors);
         foreigner.setDirector(martin);
         foreigner.setImage(Resources.getResource("foreigner-img-base64.txt").toString());
-        foreigner.setImage("ggg");
         foreignerId = movieFacade.createMovie(foreigner);
 
         wonderWoman = new CreateMovieDto();
         wonderWoman.setTitle("Wonder Woman");
         wonderWoman.setDescription("When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.");
-        wonderWoman.setDescription("When a pilot crashes and tells of conflict in the outside world.");
         wonderWoman.setDescription("dscr");
-        wonderWoman.setDateOfRelease(LocalDate.of(2017,6,1));
+        wonderWoman.setDateOfRelease(LocalDate.of(2017, 6, 1));
         genres = new ArrayList<>();
         genres.add(action);
         genres.add(adventure);
@@ -160,7 +151,6 @@ public class DatabaseSeeder {
         wonderWoman.setActors(actors);
         wonderWoman.setDirector(patty);
         wonderWoman.setImage(Resources.getResource("wonder-woman-img-base64.txt").toString());
-        wonderWoman.setImage("gg");
         wonderWomanId = movieFacade.createMovie(wonderWoman);
     }
 
@@ -183,5 +173,4 @@ public class DatabaseSeeder {
         pepaRating.setDescription("Didn't like it at all");
         pepaRating.setId(ratingFacade.create(pepaRating));
     }
-
 }

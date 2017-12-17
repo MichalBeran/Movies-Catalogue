007ac92e4fe019c6405781700b967bacf739dcf1
Marek Urban <marek@Mareks-MacBook-Pro.local>
2017-12-17 16:28:44 +0100

    Refactor DirectorsController

--- Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/DirectorsController.java
+++ Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/DirectorsController.java
@@ -1,15 +1,11 @@
 package cz.muni.fi.pa165.rest.controllers;
 
-import cz.muni.fi.pa165.dto.ActorDto;
 import cz.muni.fi.pa165.dto.DirectorDto;
-import cz.muni.fi.pa165.dto.GenreDto;
-import cz.muni.fi.pa165.facade.ActorFacade;
 import cz.muni.fi.pa165.facade.DirectorFacade;
-import cz.muni.fi.pa165.facade.GenreFacade;
 import cz.muni.fi.pa165.rest.Api;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javassist.tools.web.BadHttpRequest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
@@ -19,63 +15,56 @@ import java.util.List;
 @RequestMapping(Api.ROOT_URI_DIRECTORS)
 public class DirectorsController {
 
-    final static Logger logger = LoggerFactory.getLogger(DirectorsController.class);
-
-    //TODO: test for bugs, certainly the methods will throw some exceptions for null pointers
-
     @Inject
     private DirectorFacade directorFacade;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<DirectorDto> index() {
-        return directorFacade.findAll();
+    public ResponseEntity<List<DirectorDto>> index() {
+        return ResponseEntity.ok(directorFacade.findAll());
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public final DirectorDto create(@RequestBody DirectorDto dto) throws Exception {
+    public final ResponseEntity<DirectorDto> create(@RequestBody DirectorDto dto) throws Exception {
         try {
-            logger.debug("dto incoming: "+dto.toString());
             Long id = directorFacade.create(dto);
-            return directorFacade.findById(id);
+            return ResponseEntity.ok(directorFacade.findById(id));
         } catch (Exception ex) {
-            throw new Exception(ex.getMessage(),ex);
+            return ResponseEntity.badRequest().build();
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public DirectorDto get(@PathVariable("id") Long id) throws Exception {
+    public ResponseEntity<DirectorDto> get(@PathVariable("id") Long id) throws Exception {
         DirectorDto dto = directorFacade.findById(id);
         if (dto == null) {
-            // TODO: add exceptions to the project
-            throw new Exception("not found");
+            return ResponseEntity.notFound().build();
         }
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public final DirectorDto update(@PathVariable("id") Long id, @RequestBody DirectorDto dto) throws Exception {
+    public final ResponseEntity<DirectorDto> update(@PathVariable("id") Long id, @RequestBody DirectorDto dto) throws Exception {
         try {
-            DirectorDto stored = directorFacade.findById(id);
-            stored.setFirstName(!dto.getFirstName().equals("") ? dto.getFirstName() : stored.getFirstName());
-            stored.setLastName(!dto.getLastName().equals("") ? dto.getLastName() : stored.getFirstName());
-
-            return directorFacade.update(stored);
+            if(!dto.getId().equals(id)){
+                throw new Exception("Ids don't match.");
+            }
+            return ResponseEntity.ok(directorFacade.update(dto));
         } catch (Exception ex) {
-            throw new Exception(ex.getMessage(),ex);
+            return ResponseEntity.badRequest().build();
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public final void delete(@PathVariable("id") Long id) throws Exception {
+    public final ResponseEntity delete(@PathVariable("id") Long id) throws Exception {
         try {
             DirectorDto stored = directorFacade.findById(id);
             directorFacade.delete(stored);
+            return ResponseEntity.noContent().build();
         } catch (Exception ex) {
-            throw new Exception("does not exist");
+            return ResponseEntity.notFound().build();
         }
     }
-
 }

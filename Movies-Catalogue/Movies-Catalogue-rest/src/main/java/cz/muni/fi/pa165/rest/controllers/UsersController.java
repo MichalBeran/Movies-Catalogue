18157e1c3bab077f0d8cea5063f9914d66d40ea9
MichalBeran <beran.mi.g@gmail.com>
2017-12-18 20:24:57 +0100

    Added response entity in users controller. (#144)

--- Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/UsersController.java
+++ Movies-Catalogue/Movies-Catalogue-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/UsersController.java
@@ -27,30 +27,30 @@ public class UsersController {
     private UserFacade userFacade;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<UserDto> index() {
+    public final ResponseEntity<List<UserDto>> index() {
         List<UserDto> list = userFacade.findAllUsers();
         for(int i = 0; i<list.size(); i++){
             UserDto u = list.get(i);
             u.setAdmin(userFacade.isAdmin(u));
             list.set(i, u);
         }
-        return list;
+        return ResponseEntity.ok(list);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public final UserDto register(@RequestBody UserDto dto)throws Exception{
+    public final ResponseEntity<UserDto> register(@RequestBody UserDto dto)throws Exception{
         try {
             logger.debug("dto incoming: "+dto.toString());
             Long id = userFacade.registerUser(dto, dto.getPassword());
-            return userFacade.findUserById(id);
+            return ResponseEntity.ok(userFacade.findUserById(id));
         } catch (Exception ex) {
-            throw new Exception(ex.getMessage(),ex);
+            return ResponseEntity.badRequest().build();
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDto> get(@PathVariable("id") Long id) throws Exception {
+    public final ResponseEntity<UserDto> get(@PathVariable("id") Long id) throws Exception {
         UserDto userDto = userFacade.findUserById(id);
         if(userDto == null){
             return ResponseEntity.notFound().build();
@@ -59,25 +59,25 @@ public class UsersController {
     }
 
     @RequestMapping(value="/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserDto login(@RequestBody UserLoginDto dto) throws Exception {
+    public final ResponseEntity<UserDto> login(@RequestBody UserLoginDto dto) throws Exception {
         UserDto loginUser = new UserDto();
         loginUser.setMail(dto.getMail());
         UserDto userDto = userFacade.signIn(loginUser, dto.getPassword());
         if(userDto == null){
-            throw new Exception("LOGIN FAILED");
+            return ResponseEntity.badRequest().build();
         }
         userDto.setAdmin(userFacade.isAdmin(userDto));
-        return userDto;
+        return ResponseEntity.ok(userDto);
     }
 
     @RequestMapping(value="/makeAdmin", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public boolean makeAdmin(@RequestBody UserDto dto) throws Exception {
+    public final boolean makeAdmin(@RequestBody UserDto dto) throws Exception {
         userFacade.makeAdmin(dto);
         return userFacade.isAdmin(dto);
     }
 
     @RequestMapping(value="/unmakeAdmin", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public boolean unmakeAdmin(@RequestBody UserDto dto) throws Exception {
+    public final boolean unmakeAdmin(@RequestBody UserDto dto) throws Exception {
         userFacade.unmakeAdmin(dto);
         return userFacade.isAdmin(dto);
     }

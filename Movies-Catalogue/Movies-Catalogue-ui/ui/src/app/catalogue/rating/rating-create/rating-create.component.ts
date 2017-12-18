4a17e87b1e4d6a92227b248c04fc1f72abb156fb
Marek Urban <marek@Mareks-MacBook-Pro.local>
2017-12-18 17:37:24 +0100

    create base rating UI implementation

--- Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/catalogue/rating/rating-create/rating-create.component.ts
+++ Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/catalogue/rating/rating-create/rating-create.component.ts
@@ -1,39 +1,65 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {AppHelper} from "../../../app.helper";
 import {Rating} from "../../../models/rating.model";
+import {RatingCommonComponent} from "../rating.common.component";
+import {RestService} from "../../../services/rest.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 declare const Slider: any;
 declare const $: any;
+
 @Component({
   selector: 'app-rating-create',
   templateUrl: './rating-create.component.html',
-  styleUrls: ['./rating-create.component.css']
+  styleUrls: ['./rating-create.component.less']
 })
-export class RatingCreateComponent implements OnInit, AfterViewInit {
+export class RatingCreateComponent extends RatingCommonComponent implements OnInit, AfterViewInit {
 
   invalid = AppHelper.invalid;
   rating: Rating = new Rating();
 
   ngAfterViewInit(): void {
-    this.initOverallSlider();
+    this.initSliders();
   }
 
-  constructor() { }
+  constructor(protected service: RestService, protected router: Router, private route: ActivatedRoute) {
+    super(service, router);
+  }
 
   ngOnInit() {
   }
 
-  initOverallSlider() {
+  initSliders() {
     const overall = $('#overall');
-    overall.slider({
-      formatter: function(value) {
-        return 'Current value: ' + value;
-      }
-    });
-    overall.on('slide', function(slideEvent){
+    overall.slider();
+    overall.on('slide', function (slideEvent) {
       $('#overall-val').text(slideEvent.value);
     });
 
+    const actors = $('#actors');
+    actors.slider();
+    actors.on('slide', function (slideEvent) {
+      $('#actors-val').text(slideEvent.value);
+    });
 
+    const scenario = $('#scenario');
+    scenario.slider();
+    scenario.on('slide', function (slideEvent) {
+      $('#scenario-val').text(slideEvent.value);
+    });
+
+
+  }
+
+  toggleNewRatingForm() {
+    $('.rating-create-container').slideToggle(500);
+  }
+
+  save() {
+    this.service.create(this.rating).subscribe(result => this.refresh());
+  }
+
+  refresh() {
+    this.router.navigateByUrl(this.route.snapshot.toString());
   }
 }

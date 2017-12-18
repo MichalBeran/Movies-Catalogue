e44a2b595304e67e94da85c2047ab450d1496091
Marek Urban <marek@Mareks-MacBook-Pro.local>
2017-12-18 15:05:30 +0100

    create base rating form

--- Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/catalogue/rating/rating-create/rating-create.component.ts
+++ Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/catalogue/rating/rating-create/rating-create.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AppHelper} from "../../../app.helper";
+import {Rating} from "../../../models/rating.model";
 
+declare const Slider: any;
+declare const $: any;
 @Component({
   selector: 'app-rating-create',
   templateUrl: './rating-create.component.html',
   styleUrls: ['./rating-create.component.css']
 })
-export class RatingCreateComponent implements OnInit {
+export class RatingCreateComponent implements OnInit, AfterViewInit {
+
+  invalid = AppHelper.invalid;
+  rating: Rating = new Rating();
+
+  ngAfterViewInit(): void {
+    this.initOverallSlider();
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  initOverallSlider() {
+    const overall = $('#overall');
+    overall.slider({
+      formatter: function(value) {
+        return 'Current value: ' + value;
+      }
+    });
+    overall.on('slide', function(slideEvent){
+      $('#overall-val').text(slideEvent.value);
+    });
+
+
+  }
 }

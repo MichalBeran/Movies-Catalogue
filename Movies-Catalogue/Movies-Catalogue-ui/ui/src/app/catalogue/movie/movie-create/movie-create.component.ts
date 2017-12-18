62a988ee178ae584a5d41eba50ac18334c0a05ab
MichalBeran <beran.michal@mail.muni.cz>
2017-12-18 15:03:07 +0100

    Modified movie create cmponent.

--- Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/catalogue/movie/movie-create/movie-create.component.ts
+++ Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/catalogue/movie/movie-create/movie-create.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import {MovieCommonComponent} from "../movie.common.component";
+import {RestService} from "../../../services/rest.service";
+import {ActivatedRoute, Router} from '@angular/router';
+import {Movie} from "../../../models/movie.model";
+import {isNullOrUndefined} from 'util';
 
 @Component({
   selector: 'app-movie-create',
   templateUrl: './movie-create.component.html',
   styleUrls: ['./movie-create.component.css']
 })
-export class MovieCreateComponent implements OnInit {
+export class MovieCreateComponent extends MovieCommonComponent implements OnInit {
 
-  constructor() { }
+  movie: Movie = new Movie();
+  files : FileList;
 
-  ngOnInit() {
+  title = 'Create';
+  editing = false;
+
+  constructor(protected service: RestService, protected router: Router, private route: ActivatedRoute){
+    super(service, router);
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (!isNullOrUndefined(params['id'])) {
+        this.service.find(params['id']).subscribe(res => this.setFormForEdit(res));
+      }
+    });
+  }
+
+  save() {
+    if (isNullOrUndefined(this.movie.id)) {
+      // create
+      this.service.create(this.movie).subscribe(result => this.redirect(result));
+    } else {
+      // update
+      this.service.update(this.movie).subscribe(result => this.redirect(result));
+    }
+  }
+
+  redirect(created: Movie) {
+    this.router.navigateByUrl(`movies/detail/${created.id}`);
+  }
+
+  remove(id) {
+    super.delete(id, () => super.toIndexPage());
+  }
+
+  private setFormForEdit(movie: Movie) {
+    this.title = `Update`;
+    this.movie = movie;
+    this.editing = true;
+  }
+
+
 }
+

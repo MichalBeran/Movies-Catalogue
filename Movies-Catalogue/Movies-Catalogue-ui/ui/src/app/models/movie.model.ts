6bae93f63556cdb87661cfe3e7e0108968bb9b6a
Marek Urban <marek@Mareks-MacBook-Pro.local>
2017-12-18 14:59:59 +0100

    Add default constructor for movie model

--- Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/models/movie.model.ts
+++ Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/models/movie.model.ts
@@ -1,14 +1,25 @@
 import {BaseModel} from './base.model';
 
 export class Movie extends BaseModel {
-id?: number;
-title: string;
-description: string;
-image: string;
-dateOfRelease: {
-  year: string,
-  monthValue: string,
-  dayOfMonth: string
-};
+  id?: number;
+  title: string;
+  description: string;
+  image: string;
+  dateOfRelease: {
+    year: string,
+    monthValue: string,
+    dayOfMonth: string
+  };
 
+  constructor() {
+    super();
+    this.title = '';
+    this.description = '';
+    this.image = '';
+    this.dateOfRelease = {
+      year: '',
+      monthValue: '',
+      dayOfMonth: ''
+    };
+  }
 }

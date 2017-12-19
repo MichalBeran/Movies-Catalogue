78364936030b6860e06a5aed20b28b269c44f396
Dominik Mlynka <dominik.mlynka@gmail.com>
2017-12-19 02:34:50 +0100

    RecommendedMovieService created

---
+++ Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/services/recommended.movies.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {RestService} from "./rest.service";
+import 'rxjs/add/operator/map';
+import {Movie} from "../models/movie.model";
+
+
+const HttpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
+
+@Injectable()
+export class RecomMoviesService extends RestService {
+
+  public token: string;
+
+  constructor(protected http: HttpClient) {
+    super(http);
+
+    // const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  find(id): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.repository}/movies/recom/${id}`);
+  }
+
+}

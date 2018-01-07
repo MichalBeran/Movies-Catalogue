04dd182f91515af10e5bc2071aec5ed43d7d19c6
arrynn <umarekk@gmail.com>
2018-01-07 21:24:10 +0100

    added new logo

--- Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/template/header/header.component.ts
+++ Movies-Catalogue/Movies-Catalogue-ui/ui/src/app/template/header/header.component.ts
@@ -27,6 +27,16 @@ export class HeaderComponent implements OnInit, AfterViewInit {
     $('.dropdown-container a').click(function(){
       that.dropdownMenuToggle(null);
     })
+
+    setInterval(function(){
+      const date = new Date();
+      const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+      const mins = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+      const sec = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+      const mils = Math.floor(date.getMilliseconds() / 10) < 10 ? '0' + Math.floor(date.getMilliseconds() / 10) : Math.floor(date.getMilliseconds() / 10);
+      const text = hours + ':' + mins + ':' + sec + '.' + mils;
+    $('.logo-bg-timer').text(text);
+    }, 91);
   }
 
   getLoggedUser() {
